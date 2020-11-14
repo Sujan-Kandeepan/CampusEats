cbdeec775e16d5ea350ce2bdee0f61bc26633ae9
Sujan Kandeepan <kandeeps@mcmaster.ca>
2020-11-14 18:14:09 -0500

    Get latest App.js from master

--- src/App.js
+++ src/App.js
@@ -15,37 +15,60 @@ import Map from "./Map.js";
 import Main from "./Main.js";
 import Details from "./Details.js";
 import AccSetup from "./AccSetup.js";
-import Settings from "./Settings.js";
+import Login from "./Login.js";
 
-function App() {
-  return (
-    <Router>
-        <Switch>
-          <Route path="/details">
-            <Details />
-          </Route>
-          <Route path="/map">
-            <Map/>
-          </Route>
-          <Route path="/review">
-            <Review />
-          </Route>
-          <Route path="/search">
-            <Search />
-          </Route>
-          <Route path="/accSetup">
-            <AccSetup />
-        </Route>
-        <Route path="/settings">
-          <Settings />
-        </Route>
-          {/* Route path of "/" must be last as it matches all routes */}
-          <Route path="/">
-            <Main />
-          </Route>
-        </Switch>
-    </Router>
-  );
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: {
+        username: "",
+        password: ""
+      },
+      loginState: false,
+    }
+    this.changeLoginStateFromApp = this.changeLoginStateFromApp.bind(this);
+  }
+
+  changeLoginStateFromApp(to) {
+      this.setState({
+        loginState: to
+      })
+  }
+
+  render() {
+    window.state = this.state;
+    return (
+      <Router>
+          <Switch>
+            <Route path="/details">
+              <Details />
+            </Route>
+            <Route path="/map">
+              <Map/>
+            </Route>
+            <Route path="/review">
+              <Review />
+            </Route>
+            <Route path="/search">
+              <Search />
+            </Route>
+            <Route path="/accSetup">
+              <AccSetup />
+            </Route>
+            <Route path="/login">
+              <Login username={this.state.user.username} password={this.state.user.password} changeLoginState={this.changeLoginStateFromApp} loginState={this.state.loginState}/>
+            </Route>
+            {/* Route path of "/" must be last as it matches all routes */}
+            <Route path="/">
+              <Main />
+            </Route>
+          </Switch>
+      </Router>
+    );
+  }
+  
 }
 
-export default App;
