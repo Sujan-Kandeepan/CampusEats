b19ae67d49617ca0265a99c63092952081c48d54
Sujan Kandeepan <kandeeps@mcmaster.ca>
2020-11-16 10:22:49 -0500

    Tweaked footer

--- src/component/footer.js
+++ src/component/footer.js
@@ -7,24 +7,19 @@ import { Link } from "react-router-dom";
 
 export default class Footer extends React.Component {
     render() {
-        return(
+        return (
             <React.Fragment>
-                
-                 {/* Footer template from Bulma: https://bulma.io/documentation/layout/footer/ */}
-
+                {/* Footer template from Bulma: https://bulma.io/documentation/layout/footer/ */}
                 <footer className="footer">
                     <div className="content">
-                        <div className="columns" style={{ height: "30px", padding: "10px 0" }}>
+                        <div className="columns is-vcentered" style={{ height: "30px", padding: "10px 0" }}>
                             {/* <Link to="/">About</Link> */}
                             {/* <Link to="/">Privacy Policy</Link>  */}
                             <Link to="/contactSupport" style={{ marginLeft: "auto" }}>Contact Support</Link>
                             {/* <Link to="/">Report a problem</Link> */}
                         </div>
-                    <p>
-                        
-                    </p>
                     </div>
-                    </footer>
+                </footer>
             </React.Fragment>
         )
     }
